c3782766696fd8a02dd97c4e410919ab1166dae7
zDORROz <114201872+zDORROz@users.noreply.github.com>
2025-06-17 03:33:23 +0200

    Update ijvm_struct.h

--- include/ijvm_struct.h
+++ include/ijvm_struct.h
@@ -1,41 +1,32 @@
-
 #ifndef IJVM_STRUCT_H
 #define IJVM_STRUCT_H
 
-#include <stdio.h>  /* contains type FILE * */
-
+#include <stdio.h>
+#include <stdbool.h>
 #include "ijvm_types.h"
-/**
- * All the state of your IJVM machine goes in this struct!
- **/
 
 typedef struct {
-    int *elements;
+    word_t *elements;
     int top;
     int capacity;
 } Stack;
 
 typedef struct IJVM {
-    // do not changes these two variables
-    FILE *in;   // use fgetc(ijvm->in) to get a character from in.
-                // This will return EOF if no char is available.
-    FILE *out;  // use for example fprintf(ijvm->out, "%c", value); to print value to out
+    FILE *in;
+    FILE *out;
 
-  // your variables go here
-    #define EXPECTED_MAGIC_NUMBER 0x1DEADFAD
-    uint32_t magic_number;
+    byte_t *text;
+    word_t *constant_pool;
+    
+    // Add constant_pool_size to the struct
     uint32_t constant_pool_size;
     uint32_t text_size;
-    uint32_t* constant_pool;
-    uint8_t* text;
+    
     unsigned int program_counter;
-    Stack* stack;
-    bool halted;
-    int16_t offset;
-    int local_variable_count;
     int lv_pointer;
-    int stack_pointer;
-    uint16_t method_index;
+
+    Stack *stack;
+    bool halted;
 } ijvm;
 
-#endif 
+#endif
