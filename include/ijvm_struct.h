bd0989d0db9f78db25a2b498576498fda2474aa2
zDORROz <114201872+zDORROz@users.noreply.github.com>
2025-06-20 00:22:36 +0200

    Update ijvm_struct.h

--- include/ijvm_struct.h
+++ include/ijvm_struct.h
@@ -5,6 +5,16 @@
 #include <stdbool.h>
 #include "ijvm_types.h"
 
+/**
+ * @brief Represents a single allocated array on the heap.
+ */
+typedef struct {
+    word_t reference;  // Unique identifier for this heap object
+    word_t* data;      // The actual array data
+    int size;          // The number of elements in the array
+    bool marked;       // Flag for the garbage collector's mark phase
+} heap_object_t;
+
 typedef struct {
     word_t *elements;
     int top;
@@ -27,6 +37,17 @@ typedef struct IJVM {
 
     Stack *stack;
     bool halted;
+
+    // --- Heap Management ---
+    heap_object_t** heap; // A dynamic array of pointers to heap objects
+    int heap_size;         // Current number of objects on the heap
+    int heap_capacity;     // Current allocated capacity of the heap array
+    word_t next_ref;       // Counter to generate unique array references
+
+    // --- GC Tracking for Tests ---
+    word_t* freed_refs;    // Stores references freed in the last GC cycle
+    int freed_refs_size;   // Number of references in freed_refs
+    int freed_refs_capacity; // Allocated capacity for freed_refs
 } ijvm;
 
 #endif
